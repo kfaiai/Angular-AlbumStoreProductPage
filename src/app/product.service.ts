32b4985598b0327775a143bdac46ef6f50c3531b
kfaiai <49914190+kfaiai@users.noreply.github.com>
2019-11-12 10:49:21 -0500

    fix call to json file

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,4 +3,12 @@ import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  private _albumUrl: "../assests/album.json";
+
+  constructor(private _http: Http) {}
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json());
+  }
+}
