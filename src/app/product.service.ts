715445c9a621d8c80b8ad2b511621cd0a7fbccaa
kfaiai <49914190+kfaiai@users.noreply.github.com>
2019-11-12 10:25:24 -0500

    removed product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,4 @@ import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 
 @Injectable()
-export class ProductService {
-  private _albumUrl: "../assets/album.json";
-
-  constructor(private _http: Http) {}
-
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
-  }
-}
+export class ProductService {}
